c157233baac3ce1493f36f920988c1184c94e1af
Evan Dufraisse <edufraisse@gmail.com>
2024-12-28 10:13:30 +0100

    new: support for datatable upon page change

--- public/scripts/table-init.js
+++ public/scripts/table-init.js
@@ -1,11 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
+function initializeTable() {
+    if (typeof $ === 'undefined' || typeof $.fn.DataTable === 'undefined') {
+        console.error('jQuery or DataTables is not loaded!');
+        return;
+    }
+
     document.querySelectorAll('table').forEach(table => {
-        table.classList.add('display');
+        if (!table.classList.contains('display')) {
+            table.classList.add('display'); // Add DataTables required class
+        }
     });
-    $('table').DataTable({
-        paging: false,
-        searching: false,
-        scrollX: true,
+
+    document.querySelectorAll('table.display').forEach(table => {
+        if (!$.fn.dataTable.isDataTable(table)) {
+            $(table).DataTable({
+                paging: false,
+                searching: false,
+                scrollX: true,
+            });
+        }
     });
+}
+
+
+
+// $('table').DataTable({
+//     paging: false,
+//     searching: false,
+//     scrollX: true,
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    initializeTable();
 });
 
+document.addEventListener('astro:after-swap', () => {
+    initializeTable();
+});
+
+initializeTable();
